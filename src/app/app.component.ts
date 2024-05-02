e58647449245b45db8ae40439454a22e83e6feeb
chocho77 <chavmom@gmail.com>
2024-05-02 08:46:01 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/app.component.ts 	new file:   src/app/employee.service.spec.ts 	new file:   src/app/employee.service.ts  Changes not staged for commit: 	modified:   src/app/employee-list/employee-list.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from "./employee-list/employee-list.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, EmployeeListComponent]
+    imports: [RouterOutlet, EmployeeListComponent,HttpClientModule]
 })
 export class AppComponent {
   title = 'angular-frontend';
