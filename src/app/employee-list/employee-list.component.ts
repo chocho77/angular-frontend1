12365d17a785153f842442de0393acee84b81173
chocho77 <chavmom@gmail.com>
2024-05-02 08:46:26 +0300

    On branch master  Changes to be committed: 	modified:   src/app/employee-list/employee-list.component.ts

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
 import { NgFor } from '@angular/common';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -13,22 +14,16 @@ export class EmployeeListComponent implements OnInit{
 
   employees!: Employee[];
   
-  constructor(){ }
+  constructor(private employeeService: EmployeeService){ }
 
   ngOnInit(): void {
-    this.employees = [{
-      "id": 1,
-      "firstName": 'dakarta',
-      "lastName": "ramesh",
-      "emailId": "ramesh@gmail.com",  
-    },
-    {
-      "id": 2,
-      "firstName": 'dakarta',
-      "lastName": "ramesh",
-      "emailId": "ramesh@gmail.com",  
-    }];
-      
+     this.getEmployees();    
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data =>{
+      this.employees = data;
+    })
   }
   
   
