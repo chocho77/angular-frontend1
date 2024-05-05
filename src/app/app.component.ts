7ca1074809b20a152056a80012dff6396002b28b
chocho77 <chavmom@gmail.com>
2024-05-05 23:47:32 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/app.component.ts 	modified:   src/app/employee-list/employee-list.component.html 	modified:   src/app/employee.ts 	modified:   src/main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from "./employee-list/employee-list.component";
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, EmployeeListComponent,HttpClientModule]
+    imports: [RouterOutlet, EmployeeListComponent]
 })
 export class AppComponent {
   title = 'angular-frontend';
